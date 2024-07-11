0a3832ac183893e26f0af2001edf55acde8c5bfc
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-07-11 23:52:31 +0530

    11 July assignment
    Count Space in string
    String Palindrome

---
+++ 11July/CountSpace.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSpaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            String stringInput = Console.ReadLine();
+            Console.WriteLine(CountSpace(stringInput));
+            
+        }
+
+        static int CountSpace(String xyz)
+        {
+            int count = 0;
+            foreach (char c in xyz)
+            {
+                if(c == ' ') 
+                    count++;
+            }
+            return count;
+
+        }
+
+            
+    }
+}
---
+++ 11July/StringPalindrome.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String: ");
+            string stringInput = Console.ReadLine();
+            Console.WriteLine(CheckStringPalindrome(stringInput));
+        }
+        static string CheckStringPalindrome(string input)
+        {
+            String inputString = input.ToLower();
+
+            char[] originalString = inputString.ToCharArray();
+            Array.Reverse(originalString);
+            
+            string reverseString = new string(originalString);
+
+            if(inputString == reverseString)
+            {
+                return "String is Palindrome";
+            }
+            return  "String is not a Palindrome";
+            
+            
+
+
+        }
+    }
+}
