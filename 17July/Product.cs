6843b3043ca8398c8b67410409729c7b2735d4db
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-07-17 21:03:59 +0530

    Add files via upload

---
+++ 17July/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.cs
+{
+    public class Product
+    {
+        private int _id;
+        private string _name;
+        private double _price;
+        private double _discountPercentage;
+
+        
+
+        public Product(int productId, string productName, double productPrice):this(productId, productName, productPrice, 0) 
+        {
+        }
+        public Product(int productId, string productName, double productPrice, double discountPercent)
+        {
+            _id =productId ;
+            _name = productName;
+            _price = productPrice;
+            _discountPercentage = discountPercent;
+            
+        }
+        public static Product PrintDetails(Product[] products, int productId)
+        {
+            foreach (var product in products)
+            {
+                if (product._id == productId)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+
+        public static double DiscountAmount(Product[] products, int productId)
+        {
+            Product product = PrintDetails(products, productId);
+            double discountAmount = (product._discountPercentage / 100) * product._price;
+            double priceAfterDiscount = product._price - discountAmount;
+            return priceAfterDiscount;
+        }
+
+        public override string ToString()
+        {
+            return "ProductId : " + _id + "\nProductname : " + _name + "\nProduct Price " + _price;
+        }
+    }
+
+    
+}
---
+++ 17July/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] product = new Product[]
+            {                 
+                new Product(1,"Jeans", 2000,6),
+                new Product(2,"Trouser",800,5)
+            };
+
+            Console.WriteLine("Enter a product id");
+            int productId = int.Parse(Console.ReadLine());
+            Product selectedProduct = (Product.PrintDetails(product, productId));
+            Console.WriteLine(selectedProduct);
+            Console.WriteLine($"Final price after discount : " + Product.DiscountAmount(product, productId));
+
+        }
+    }
+}
