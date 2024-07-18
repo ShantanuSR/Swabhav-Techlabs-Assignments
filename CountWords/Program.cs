cf21b6889b0d11412e9c7fe3c867e6541043c8ab
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-07-18 22:41:19 +0530

    Add files via upload

---
+++ CountWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string : ");
+            string inputString = Console.ReadLine();
+            string[] inputArray = inputString.Split( new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries );
+            Console.WriteLine("Number of words : " + inputArray.Length);
+        }
+    }
+}
