b91105ae25ee87e10db3cb431ccd367ec804e0e7
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-07-18 23:29:03 +0530

    Player App

---
+++ PlayerApp/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    class Player
+    {
+        private int _playerId { get; set; }
+        private string _playerName {  get; set; }
+        private int _playerAge{  get; set; }
+
+        public Player(int playerId,string playerName):this(playerId,playerName,0)
+        {
+
+        }
+
+        public Player(int playerId, string playerName,int playerAge) 
+        {
+            _playerId = playerId;
+            _playerName = playerName;
+            _playerAge = playerAge;
+        }
+
+        public static string WhoIsElder(Player[] players)
+        {
+            int maxAge = 0;
+            int elderPlayerId=0;
+            string nameOfPlayer = " ";
+            foreach (Player player in players)
+            {
+                if (maxAge < player._playerAge)
+                {
+                    maxAge = player._playerAge;
+                    elderPlayerId= player._playerId;
+                    nameOfPlayer = player._playerName;
+                }    
+            }
+            return ("Max age : "+ maxAge + "\nPlayer Id : "+elderPlayerId+"\nPlayer name : "+nameOfPlayer);
+        }
+    }
+}
---
+++ PlayerApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = new Player[3]
+            {
+                new Player(1,"Shantanu"),
+                new Player(2,"Prasad",27),
+                new Player(3,"Sarvesh",25)
+            };
+
+            Console.WriteLine(Player.WhoIsElder(players));
+
+        }
+    }
+}
