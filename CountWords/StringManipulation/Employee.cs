88d29099c65b5fa315510ca1e6862195a2f9b19d
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-07-18 22:46:19 +0530

    Add files via upload

---
+++ CountWords/StringManipulation/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    public class Employee
+    {
+        private string _employeeName;
+        private string _companyName;
+        private string _companyLocation;
+
+        public Employee(string employeeName,string companyName,string companyLocation)
+        {
+            _employeeName = employeeName;  
+            _companyName = companyName;
+            _companyLocation = companyLocation;
+        }
+
+        public  string PrintDetails()
+        {
+            return ("Developer name is :" + _employeeName + "\nCompany name is : "+_companyName+"\nLocation : "+_companyLocation);
+        }
+    }
+}
---
+++ CountWords/StringManipulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Shantanu","Aurionpro","Pune");
+            Employee employee1 = new Employee("Prasad","Accenture","Airoli");
+            Employee employee2 = new Employee("Sarvesh","TCS","Thane");
+            PrintEmployee(employee);
+            PrintEmployee(employee1);
+            PrintEmployee(employee2);
+            
+        }
+
+        public static void PrintEmployee(Employee employee)
+        {
+            Console.WriteLine();
+            Console.WriteLine(employee.PrintDetails());
+        }
+    }
+}
