867f9ba1bcafd81c9c3ad290a2f79c04d789ab95
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-07-20 21:41:22 +0530

    Currency Calculator

---
+++ Currency Calculator/CalculateCurrency.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_Currency
+{
+    internal class CalculateCurrency
+    {
+        public void Currency(int amount)
+        {
+            int[] NotesArray = new int[4] { 2000, 500, 200, 100 };
+            string[] NotesArray2 = new string[4] { "Two Thousand ", "Five Hundred ", "Two Hundred", "Hundred" };
+
+            int[] NotesCounterArray = new int[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (amount >= NotesArray[i])
+                {
+                    NotesCounterArray[i] = amount / NotesArray[i];
+                    amount = amount % NotesArray[i];
+                }
+            }
+
+            
+            for (int i = 0; i < 4; i++)
+            {
+                if (NotesCounterArray[i] != 0)
+                {
+                    Console.WriteLine(NotesArray2[i] + " : " + NotesCounterArray[i]);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Currency Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_Currency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateCurrency calculateCurrency = new CalculateCurrency();
+            Console.WriteLine("Enter the withdrawal amount : ");
+            int inputAmount = int.Parse(Console.ReadLine());
+
+            if (inputAmount <= 50000 && inputAmount%100==0)
+                calculateCurrency.Currency(inputAmount);
+
+            else if(inputAmount % 100 != 0)
+                Console.WriteLine("Input amount should be multiple of 100");
+
+            else
+               Console.WriteLine("Amount exceeds withdrwal limit of 50000");
+        }
+    }
+}
