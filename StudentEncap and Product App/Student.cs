9984ae2839e0314faeb0d7995740489521377951
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-07-20 23:51:15 +0530

    Student Encapsulation and Product app

---
+++ StudentEncap and Product App/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+namespace StudentEncapsulation
+{
+    internal class Student
+    {
+        private int _stuRollNum;
+        private string _stuName;
+        private float _stuCgpa;
+        public int GetStudentRollNumber()
+        {
+            return _stuRollNum;
+        }
+        public string GetStudentName()
+        {
+            return _stuName;
+        }
+        public float GetStudentCgpa()
+        {
+            return _stuCgpa;
+        }
+        public void SetStuRollNum(int rollNoOfStu)
+        {
+            if (rollNoOfStu > 0)
+            {
+                _stuRollNum = rollNoOfStu;
+            }
+            else
+            {
+                Console.WriteLine("Roll Number can't be Negative");
+                Console.WriteLine();
+            }
+        }
+        public void SetStuName(string studentName)
+        {
+            _stuName = (studentName.Length < 5) ? "" : studentName;
+        }
+        public void SetStuCgpa(float studentCgpa)
+        {
+            if (1 <= studentCgpa && studentCgpa <= 10)
+            {
+                _stuCgpa = studentCgpa;
+            }
+            else
+            {
+                Console.WriteLine("CGPA should be between 1 to 10");
+                Console.WriteLine();
+            }
+        }
+
+        public void GetPercentage()
+        {
+            Console.WriteLine("Percentage : "+_stuCgpa*9.5 + "%");
+        }
+
+    }
+}
---
+++ StudentEncap and Product App/StudentEncap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Student student = new Student();
+            Student student1 = new Student();
+
+            student.SetStuRollNum(1);
+            student.SetStuName("Rama");
+            student.SetStuCgpa(11);
+
+            
+          
+            
+            Console.WriteLine("Student Roll Number: " + student.GetStudentRollNumber());
+            Console.WriteLine("Student Name: " + student.GetStudentName());
+            Console.WriteLine("Student CGPA: " + student.GetStudentCgpa());
+
+            student.GetPercentage();
+
+        }
+    }
+}
