d90bd5db5ee690773e7000df6e8fda0f6ffa87ca
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-07-20 20:41:24 +0530

    BMI CALCULATOR

---
+++ BMI_Calculator/Person.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Calculator
+{
+     public class Person
+    {
+        public const double DEFAULT_HEIGHT=5;
+        public const double DEFAULT_WEIGHT = 50; 
+        private int _personId;
+        private string _personName;
+        private int _personAge;
+        private double _personHeight;
+        private double _personWeight;
+        public double _personBmi { get; set; }
+
+        public Person(int id ,string name,int age):this(id,name,age, DEFAULT_HEIGHT, DEFAULT_WEIGHT)
+        {
+
+        }
+
+        //public Person(int id, string name, int age, double height, double weight):this(id,name,age,height,weight,0)
+        //{
+            
+        //}
+
+        public Person(int id, string name, int age, double height, double weight)
+        {
+            _personId = id;
+            _personName = name;
+            _personAge = age;
+            _personHeight = height;
+            _personWeight = weight;
+           _personBmi = CalculateBMI();
+        }
+
+        public double CalculateBMI()
+        {
+            double mtrHeight = _personHeight * 0.3048;
+            double heightSquare = Math.Pow(mtrHeight, 2);
+            double bmi = _personWeight/heightSquare;
+            return bmi;
+           
+            
+        }
+
+        public string BodyType(double bmi)
+        {
+            if (bmi < 18.5)
+                return "Underweight";
+            else if (bmi > 18.5 && bmi < 24.9)
+                return "Normal";
+             return "Overweight";
+            
+
+        }
+
+        public static double PrintPersonBMI(Person[] persons)
+        {
+            double maxBmi = 0;
+            foreach (var person in persons)
+            {
+                if(person._personBmi > maxBmi)
+                    maxBmi = person._personBmi;
+            }
+           return maxBmi;
+        }
+
+    }
+}
---
+++ BMI_Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Person person1 = new Person(1,"Ram",20);
+           
+            Person person2 = new Person(2,"Prasad",21,5.4,68);
+            Person person3 = new Person(2,"Prathm",23,5.6,78);
+            Person person4 = new Person(2,"Shyam",24,4.9,58);
+
+            PrintBmi(person1);
+            
+            PrintBmi(person2);
+            PrintBmi(person3);
+            PrintBmi(person4);
+
+            Person[] PersonArray = new Person[4] { person1, person2, person3, person4 };
+
+            Console.WriteLine("Highest bmi : " + Person.PrintPersonBMI(PersonArray));
+
+        }
+
+        public static void PrintBmi(Person person)
+        {
+            double result = person.CalculateBMI();
+            Console.WriteLine(result);
+            Console.WriteLine(person.BodyType(result));
+            Console.WriteLine();
+
+        }
+    }
+}
