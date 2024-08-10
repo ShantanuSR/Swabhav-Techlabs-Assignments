5469e71b8808ef8c7508dca08231a8deb00e03f8
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-08-10 23:53:54 +0530

    Guitar App

---
+++ GuitarApp/GlobalENUM/Wood.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarAppAssignment.GlobalENUM
+{
+    public enum Wood
+    {
+        IndianRosewood,
+        BrazilianRosewood,
+        Mahogany,
+        Maple,
+        Alder,
+        Sitka
+    }
+}
---
+++ GuitarApp/Model/GuitarSpecs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GuitarAppAssignment.GlobalENUM;
+
+namespace GuitarAppAssignment.Model
+{
+    public class GuitarSpecs
+    {
+        public Builder Builder { get; set; }
+        public string Model { get; set; }
+        public Types Types { get; set; }
+        public Wood BackWood { get; set; }
+        public Wood TopWood { get; set; }
+        public int NumberOfStrings { get; set; }
+
+        public GuitarSpecs(Builder builder, string model, Types types, Wood backwood, Wood topWood, int numberOfStrings)
+        {
+            Builder = builder;
+            Model = model;
+            Types = types;
+            BackWood = backwood;
+            TopWood = topWood;
+            NumberOfStrings = numberOfStrings;
+        }
+    }
+}
